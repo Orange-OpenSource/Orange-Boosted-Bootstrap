7d48d5a46255c6fb202e768adc89386865a09a93
Yves Gatesoupe <yves1.gatesoupe@orange.com>
2015-12-16 11:52:06 +0100

    feat(examples): orange news
    added orange news page

--- Gruntfile.js
+++ Gruntfile.js
@@ -426,7 +426,7 @@ module.exports = function (grunt) {
       },
       docs: {
         /* boosted mod */
-        files: ['docs/assets/**/*', 'docs/_includes/**/*', '!docs/assets/**/*'],
+        files: ['docs/examples/**/*', 'docs/_includes/**/*', '!docs/assets/**/*'],
         tasks: ['dist-css', 'docs', 'jekyll:docs']
         /* end mod */
       }
