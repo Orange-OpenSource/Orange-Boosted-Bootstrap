fe777ba2e62a677ee6f22b91d40c346f2be9e38b
gildas bescond <gildas.bescond@gmail.com>
2015-12-23 11:20:25 +0100

    fix(components): modal
    Adding aria attributes

--- js/src/modal.js
+++ js/src/modal.js
@@ -513,7 +513,32 @@ const Modal = (($) => {
     Modal._jQueryInterface.call($(target), config, this)
   })
 
-
+	$(document).ready({
+	  // Malgré les recommandation de Bootstrap, on fait en sorte d'ajouter les tags aria pour "être sur"
+	  var $modals =  $('[data-toggle="modal"]');
+	  $modals.each(function() {
+	  
+		//modal = l'élement déclencheur de l'aperçu de la popin
+		//modalPanel = la fenêtre modal à proprement parler
+		var modal = $(this),
+		  modalPanel = modal.attr('data-target') ? $(modal.attr('data-target')) : $(modal.attr('href'));
+		
+		//On ajoute les tags aria qui vont bien et on empeche le focus avec tabulation
+		modalPanel.attr({ 'role' : 'dialog'});//LLA removed with BS 3.3.5, 'aria-hidden' : 'true', 'tabIndex' : '-1' });
+		
+		//On ajoute le tags aria-labelledby uniquement si la popin à un title et que celui-ci possède un id
+		var modalTitle = modalPanel.find('.modal-title');
+		if(modalTitle){ 
+		  var modalTitleId = modalTitle.attr('id');
+		  if(modalTitleId){
+			modalPanel.attr({ 'aria-labelledby' : modalTitleId });
+		  }
+		}
+	  });
+	
+	  $('.modal-dialog').attr( {'role' : 'document'})
+	 
+	})
   /**
    * ------------------------------------------------------------------------
    * jQuery
