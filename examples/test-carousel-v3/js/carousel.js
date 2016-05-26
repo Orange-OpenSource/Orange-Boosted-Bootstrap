9872259f7e29007369990b8e1f3a9bf7e1b0a85f
Yves Gatesoupe <yves1.gatesoupe@orange.com>
2016-05-26 11:11:37 +0200

    carousel demo (temporary)

--- examples/test-carousel-v3/js/carousel.js
+++ examples/test-carousel-v3/js/carousel.js
@@ -1,6 +1,15 @@
 $(function() {
     $("#main-slick-carousel").slick({
     arrows: true,
-    dots: true
+    dots: true,
+    infinite: false,
+    accessibility: true
   });
+
+});
+
+$(document).ready(function(){
+  $('.slick-slider.slick-dotted .slick-dots').attr('aria-hidden', true);
+  $('.slick-slider .slick-prev').attr('aria-label', 'Previous slide');
+  $('.slick-slider .slick-next').attr('aria-label', 'Next slide');
 });
