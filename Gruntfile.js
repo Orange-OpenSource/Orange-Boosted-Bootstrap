04c12241b58067b504d75db4000ce007e0a8e2bd
Laussel LOïc <loic.laussel@orange.com>
2016-09-08 14:07:06 +0200

    fix(package.json): update package.json for webpack compatibility

--- Gruntfile.js
+++ Gruntfile.js
@@ -93,7 +93,7 @@ module.exports = function (grunt) {
       options: {
         port: 9001,
         // change this to '0.0.0.0' to access the server from outside
-        hostname: 'localhost',
+        hostname: '0.0.0.0',
         livereload: 35730
       },
 
