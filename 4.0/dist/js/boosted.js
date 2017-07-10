840cd3860890443e52548347f8a42cdc786bc53f
Laussel LOïc <loic.laussel@orange.com>
2017-07-10 17:47:29 +0200

    update v4-dev documentation

--- 4.0/dist/js/boosted.js
+++ 4.0/dist/js/boosted.js
@@ -1402,18 +1402,8 @@ var Collapse = function ($) {
 
   $(function () {
     // local navigation
-    $('.o-nav-local .nav.collapse .nav-link').on('click', function (event) {
-      $(event.currentTarget).parent().parent().prev('.local-select').text($(event.currentTarget).text());
-    });
-
     $('.o-nav-local .nav.collapse').on('shown.bs.collapse', function (event) {
       $(event.currentTarget).find('.nav-item:first-child .nav-link').trigger('focus');
-    });
-
-    $(document).ready(function () {
-      $('.o-nav-local .local-select').each(function () {
-        $(this).text($(this).next('.nav.collapse').find('.nav-item:first-child .nav-link').text());
-      });
     });
 
     $(document).on('click', function () {
@@ -1675,8 +1665,6 @@ var Dropdown = function ($) {
     };
 
     Dropdown.prototype._getPopperConfig = function _getPopperConfig() {
-      var _this10 = this;
-
       var popperConfig = {
         placement: this._getPlacement(),
         modifiers: {
@@ -1689,14 +1677,10 @@ var Dropdown = function ($) {
         }
       };
 
+      // Disable Popper.js for Dropdown in Navbar
       if (this._inNavbar) {
-        popperConfig.modifiers.AfterApplyStyle = {
-          enabled: true,
-          order: 901, // ApplyStyle order + 1
-          fn: function fn() {
-            // reset Popper styles
-            $(_this10._menu).attr('style', '');
-          }
+        popperConfig.modifiers.applyStyle = {
+          enabled: !this._inNavbar
         };
       }
       return popperConfig;
@@ -2007,7 +1991,7 @@ var Modal = function ($) {
     };
 
     Modal.prototype.show = function show(relatedTarget) {
-      var _this11 = this;
+      var _this10 = this;
 
       if (this._isTransitioning) {
         return;
@@ -2038,24 +2022,24 @@ var Modal = function ($) {
       this._setResizeEvent();
 
       $(this._element).on(Event.CLICK_DISMISS, Selector.DATA_DISMISS, function (event) {
-        return _this11.hide(event);
+        return _this10.hide(event);
       });
 
       $(this._dialog).on(Event.MOUSEDOWN_DISMISS, function () {
-        $(_this11._element).one(Event.MOUSEUP_DISMISS, function (event) {
-          if ($(event.target).is(_this11._element)) {
-            _this11._ignoreBackdropClick = true;
+        $(_this10._element).one(Event.MOUSEUP_DISMISS, function (event) {
+          if ($(event.target).is(_this10._element)) {
+            _this10._ignoreBackdropClick = true;
           }
         });
       });
 
       this._showBackdrop(function () {
-        return _this11._showElement(relatedTarget);
+        return _this10._showElement(relatedTarget);
       });
     };
 
     Modal.prototype.hide = function hide(event) {
-      var _this12 = this;
+      var _this11 = this;
 
       if (event) {
         event.preventDefault();
@@ -2094,7 +2078,7 @@ var Modal = function ($) {
       if (transition) {
 
         $(this._element).one(Util.TRANSITION_END, function (event) {
-          return _this12._hideModal(event);
+          return _this11._hideModal(event);
         }).emulateTransitionEnd(TRANSITION_DURATION);
       } else {
         this._hideModal();
@@ -2129,7 +2113,7 @@ var Modal = function ($) {
     };
 
     Modal.prototype._showElement = function _showElement(relatedTarget) {
-      var _this13 = this;
+      var _this12 = this;
 
       var transition = Util.supportsTransitionEnd() && $(this._element).hasClass(ClassName.FADE);
 
@@ -2157,11 +2141,11 @@ var Modal = function ($) {
       });
 
       var transitionComplete = function transitionComplete() {
-        if (_this13._config.focus) {
-          _this13._element.focus();
+        if (_this12._config.focus) {
+          _this12._element.focus();
         }
-        _this13._isTransitioning = false;
-        $(_this13._element).trigger(shownEvent);
+        _this12._isTransitioning = false;
+        $(_this12._element).trigger(shownEvent);
       };
 
       if (transition) {
@@ -2172,24 +2156,24 @@ var Modal = function ($) {
     };
 
     Modal.prototype._enforceFocus = function _enforceFocus() {
-      var _this14 = this;
+      var _this13 = this;
 
       $(document).off(Event.FOCUSIN) // guard against infinite focus loop
       .on(Event.FOCUSIN, function (event) {
-        if (document !== event.target && _this14._element !== event.target && !$(_this14._element).has(event.target).length) {
-          _this14._element.focus();
+        if (document !== event.target && _this13._element !== event.target && !$(_this13._element).has(event.target).length) {
+          _this13._element.focus();
         }
       });
     };
 
     Modal.prototype._setEscapeEvent = function _setEscapeEvent() {
-      var _this15 = this;
+      var _this14 = this;
 
       if (this._isShown && this._config.keyboard) {
         $(this._element).on(Event.KEYDOWN_DISMISS, function (event) {
           if (event.which === ESCAPE_KEYCODE) {
             event.preventDefault();
-            _this15.hide();
+            _this14.hide();
           }
         });
       } else if (!this._isShown) {
@@ -2198,11 +2182,11 @@ var Modal = function ($) {
     };
 
     Modal.prototype._setResizeEvent = function _setResizeEvent() {
-      var _this16 = this;
+      var _this15 = this;
 
       if (this._isShown) {
         $(window).on(Event.RESIZE, function (event) {
-          return _this16.handleUpdate(event);
+          return _this15.handleUpdate(event);
         });
       } else {
         $(window).off(Event.RESIZE);
@@ -2210,16 +2194,16 @@ var Modal = function ($) {
     };
 
     Modal.prototype._hideModal = function _hideModal() {
-      var _this17 = this;
+      var _this16 = this;
 
       this._element.style.display = 'none';
       this._element.setAttribute('aria-hidden', true);
       this._isTransitioning = false;
       this._showBackdrop(function () {
         $(document.body).removeClass(ClassName.OPEN);
-        _this17._resetAdjustments();
-        _this17._resetScrollbar();
-        $(_this17._element).trigger(Event.HIDDEN);
+        _this16._resetAdjustments();
+        _this16._resetScrollbar();
+        $(_this16._element).trigger(Event.HIDDEN);
       });
     };
 
@@ -2231,7 +2215,7 @@ var Modal = function ($) {
     };
 
     Modal.prototype._showBackdrop = function _showBackdrop(callback) {
-      var _this18 = this;
+      var _this17 = this;
 
       var animate = $(this._element).hasClass(ClassName.FADE) ? ClassName.FADE : '';
 
@@ -2248,17 +2232,17 @@ var Modal = function ($) {
         $(this._backdrop).appendTo(document.body);
 
         $(this._element).on(Event.CLICK_DISMISS, function (event) {
-          if (_this18._ignoreBackdropClick) {
-            _this18._ignoreBackdropClick = false;
+          if (_this17._ignoreBackdropClick) {
+            _this17._ignoreBackdropClick = false;
             return;
           }
           if (event.target !== event.currentTarget) {
             return;
           }
-          if (_this18._config.backdrop === 'static') {
-            _this18._element.focus();
+          if (_this17._config.backdrop === 'static') {
+            _this17._element.focus();
           } else {
-            _this18.hide();
+            _this17.hide();
           }
         });
 
@@ -2282,7 +2266,7 @@ var Modal = function ($) {
         $(this._backdrop).removeClass(ClassName.SHOW);
 
         var callbackRemove = function callbackRemove() {
-          _this18._removeBackdrop();
+          _this17._removeBackdrop();
           if (callback) {
             callback();
           }
@@ -2326,7 +2310,7 @@ var Modal = function ($) {
     };
 
     Modal.prototype._setScrollbar = function _setScrollbar() {
-      var _this19 = this;
+      var _this18 = this;
 
       if (this._isBodyOverflowing) {
         // Note: DOMNode.style.paddingRight returns the actual value or '' if not set
@@ -2336,14 +2320,14 @@ var Modal = function ($) {
         $(Selector.FIXED_CONTENT).each(function (index, element) {
           var actualPadding = $(element)[0].style.paddingRight;
           var calculatedPadding = $(element).css('padding-right');
-          $(element).data('padding-right', actualPadding).css('padding-right', parseFloat(calculatedPadding) + _this19._scrollbarWidth + 'px');
+          $(element).data('padding-right', actualPadding).css('padding-right', parseFloat(calculatedPadding) + _this18._scrollbarWidth + 'px');
         });
 
         // Adjust navbar-toggler margin
         $(Selector.NAVBAR_TOGGLER).each(function (index, element) {
           var actualMargin = $(element)[0].style.marginRight;
           var calculatedMargin = $(element).css('margin-right');
-          $(element).data('margin-right', actualMargin).css('margin-right', parseFloat(calculatedMargin) + _this19._scrollbarWidth + 'px');
+          $(element).data('margin-right', actualMargin).css('margin-right', parseFloat(calculatedMargin) + _this18._scrollbarWidth + 'px');
         });
 
         // Adjust body padding
@@ -2460,7 +2444,7 @@ var Modal = function ($) {
    */
 
   $(document).on(Event.CLICK_DATA_API, Selector.DATA_TOGGLE, function (event) {
-    var _this20 = this;
+    var _this19 = this;
 
     var target = void 0;
     var selector = Util.getSelectorFromElement(this);
@@ -2482,8 +2466,8 @@ var Modal = function ($) {
       }
 
       $target.one(Event.HIDDEN, function () {
-        if ($(_this20).is(':visible')) {
-          _this20.focus();
+        if ($(_this19).is(':visible')) {
+          _this19.focus();
         }
       });
     });
@@ -2582,20 +2566,20 @@ var MegaMenu = function ($) {
     // private
 
     MegaMenu.prototype._addEventListeners = function _addEventListeners() {
-      var _this21 = this;
+      var _this20 = this;
 
       this._$goForwardLinks.on('click', function (event) {
-        return _this21._goForward(event);
+        return _this20._goForward(event);
       });
       this._$goBackLinks.on('click', function (event) {
-        return _this21._goBackward(event);
+        return _this20._goBackward(event);
       });
       this._$navLinks.on('keydown', function (event) {
-        return _this21._manageKeyDown(event);
+        return _this20._manageKeyDown(event);
       });
       this._$topCollapseMenus.on('shown.bs.collapse', this._collapseFocus);
       this._$navLinkCollapses.on('click', function (event) {
-        return _this21._handleCollapseToggle(event);
+        return _this20._handleCollapseToggle(event);
       });
     };
 
@@ -3181,15 +3165,15 @@ var PriorityNav = function ($) {
     };
 
     PriorityNav.prototype._bindUIActions = function _bindUIActions() {
-      var _this22 = this;
+      var _this21 = this;
 
       $(window).on(Event.RESIZE, function () {
-        _this22._$menu.addClass('resizing');
+        _this21._$menu.addClass('resizing');
 
         setTimeout(function () {
-          _this22._tearDown();
-          _this22._setupMenu();
-          _this22._$menu.removeClass('resizing');
+          _this21._tearDown();
+          _this21._setupMenu();
+          _this21._$menu.removeClass('resizing');
         }, RESIZE_DURATION);
       });
 
@@ -3584,7 +3568,7 @@ var ScrollSpy = function ($) {
 
   var ScrollSpy = function () {
     function ScrollSpy(element, config) {
-      var _this23 = this;
+      var _this22 = this;
 
       _classCallCheck(this, ScrollSpy);
 
@@ -3598,7 +3582,7 @@ var ScrollSpy = function ($) {
       this._scrollHeight = 0;
 
       $(this._scrollElement).on(Event.SCROLL, function (event) {
-        return _this23._process(event);
+        return _this22._process(event);
       });
 
       this.refresh();
@@ -3610,7 +3594,7 @@ var ScrollSpy = function ($) {
     // public
 
     ScrollSpy.prototype.refresh = function refresh() {
-      var _this24 = this;
+      var _this23 = this;
 
       var autoMethod = this._scrollElement !== this._scrollElement.window ? OffsetMethod.POSITION : OffsetMethod.OFFSET;
 
@@ -3646,8 +3630,8 @@ var ScrollSpy = function ($) {
       }).sort(function (a, b) {
         return a[0] - b[0];
       }).forEach(function (item) {
-        _this24._offsets.push(item[0]);
-        _this24._targets.push(item[1]);
+        _this23._offsets.push(item[0]);
+        _this23._targets.push(item[1]);
       });
     };
 
@@ -3904,7 +3888,7 @@ var Tab = function ($) {
     // public
 
     Tab.prototype.show = function show() {
-      var _this25 = this;
+      var _this24 = this;
 
       if (this._element.parentNode && this._element.parentNode.nodeType === Node.ELEMENT_NODE && $(this._element).hasClass(ClassName.ACTIVE) || $(this._element).hasClass(ClassName.DISABLED)) {
         return;
@@ -3946,7 +3930,7 @@ var Tab = function ($) {
 
       var complete = function complete() {
         var hiddenEvent = $.Event(Event.HIDDEN, {
-          relatedTarget: _this25._element
+          relatedTarget: _this24._element
         });
 
         var shownEvent = $.Event(Event.SHOWN, {
@@ -3954,7 +3938,7 @@ var Tab = function ($) {
         });
 
         $(previous).trigger(hiddenEvent);
-        $(_this25._element).trigger(shownEvent);
+        $(_this24._element).trigger(shownEvent);
       };
 
       if (target) {
@@ -3972,13 +3956,13 @@ var Tab = function ($) {
     // private
 
     Tab.prototype._activate = function _activate(element, container, callback) {
-      var _this26 = this;
+      var _this25 = this;
 
       var active = $(container).find(Selector.ACTIVE)[0];
       var isTransitioning = callback && Util.supportsTransitionEnd() && active && $(active).hasClass(ClassName.FADE);
 
       var complete = function complete() {
-        return _this26._transitionComplete(element, active, isTransitioning, callback);
+        return _this25._transitionComplete(element, active, isTransitioning, callback);
       };
 
       // Boosted mod
@@ -4416,7 +4400,7 @@ var Tooltip = function ($) {
     };
 
     Tooltip.prototype.show = function show() {
-      var _this27 = this;
+      var _this26 = this;
 
       if ($(this.element).css('display') === 'none') {
         throw new Error('Please use show on visible elements');
@@ -4474,11 +4458,11 @@ var Tooltip = function ($) {
           },
           onCreate: function onCreate(data) {
             if (data.originalPlacement !== data.placement) {
-              _this27._handlePopperPlacementChange(data);
+              _this26._handlePopperPlacementChange(data);
             }
           },
           onUpdate: function onUpdate(data) {
-            _this27._handlePopperPlacementChange(data);
+            _this26._handlePopperPlacementChange(data);
           }
         });
 
@@ -4493,16 +4477,16 @@ var Tooltip = function ($) {
         }
 
         var complete = function complete() {
-          if (_this27.config.animation) {
-            _this27._fixTransition();
+          if (_this26.config.animation) {
+            _this26._fixTransition();
           }
-          var prevHoverState = _this27._hoverState;
-          _this27._hoverState = null;
+          var prevHoverState = _this26._hoverState;
+          _this26._hoverState = null;
 
-          $(_this27.element).trigger(_this27.constructor.Event.SHOWN);
+          $(_this26.element).trigger(_this26.constructor.Event.SHOWN);
 
           if (prevHoverState === HoverState.OUT) {
-            _this27._leave(null, _this27);
+            _this26._leave(null, _this26);
           }
         };
 
@@ -4515,20 +4499,20 @@ var Tooltip = function ($) {
     };
 
     Tooltip.prototype.hide = function hide(callback) {
-      var _this28 = this;
+      var _this27 = this;
 
       var tip = this.getTipElement();
       var hideEvent = $.Event(this.constructor.Event.HIDE);
       var complete = function complete() {
-        if (_this28._hoverState !== HoverState.SHOW && tip.parentNode) {
+        if (_this27._hoverState !== HoverState.SHOW && tip.parentNode) {
           tip.parentNode.removeChild(tip);
         }
 
-        _this28._cleanTipClass();
-        _this28.element.removeAttribute('aria-describedby');
-        $(_this28.element).trigger(_this28.constructor.Event.HIDDEN);
-        if (_this28._popper !== null) {
-          _this28._popper.destroy();
+        _this27._cleanTipClass();
+        _this27.element.removeAttribute('aria-describedby');
+        $(_this27.element).trigger(_this27.constructor.Event.HIDDEN);
+        if (_this27._popper !== null) {
+          _this27._popper.destroy();
         }
 
         if (callback) {
@@ -4623,28 +4607,28 @@ var Tooltip = function ($) {
     };
 
     Tooltip.prototype._setListeners = function _setListeners() {
-      var _this29 = this;
+      var _this28 = this;
 
       var triggers = this.config.trigger.split(' ');
 
       triggers.forEach(function (trigger) {
         if (trigger === 'click') {
-          $(_this29.element).on(_this29.constructor.Event.CLICK, _this29.config.selector, function (event) {
-            return _this29.toggle(event);
+          $(_this28.element).on(_this28.constructor.Event.CLICK, _this28.config.selector, function (event) {
+            return _this28.toggle(event);
           });
         } else if (trigger !== Trigger.MANUAL) {
-          var eventIn = trigger === Trigger.HOVER ? _this29.constructor.Event.MOUSEENTER : _this29.constructor.Event.FOCUSIN;
-          var eventOut = trigger === Trigger.HOVER ? _this29.constructor.Event.MOUSELEAVE : _this29.constructor.Event.FOCUSOUT;
+          var eventIn = trigger === Trigger.HOVER ? _this28.constructor.Event.MOUSEENTER : _this28.constructor.Event.FOCUSIN;
+          var eventOut = trigger === Trigger.HOVER ? _this28.constructor.Event.MOUSELEAVE : _this28.constructor.Event.FOCUSOUT;
 
-          $(_this29.element).on(eventIn, _this29.config.selector, function (event) {
-            return _this29._enter(event);
-          }).on(eventOut, _this29.config.selector, function (event) {
-            return _this29._leave(event);
+          $(_this28.element).on(eventIn, _this28.config.selector, function (event) {
+            return _this28._enter(event);
+          }).on(eventOut, _this28.config.selector, function (event) {
+            return _this28._leave(event);
           });
         }
 
-        $(_this29.element).closest('.modal').on('hide.bs.modal', function () {
-          return _this29.hide();
+        $(_this28.element).closest('.modal').on('hide.bs.modal', function () {
+          return _this28.hide();
         });
       });
 
