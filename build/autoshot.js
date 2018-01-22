d073b7cc4b84a6232ce23bdaf5a0f11b28553717
Laussel LOïc <loic.laussel@orange.com>
2018-01-22 10:08:42 +0100

    doc(examples): update example page content, and update examples screenshots

--- build/autoshot.js
+++ build/autoshot.js
@@ -8,8 +8,20 @@ const serveStatic = require('serve-static')
 
 const serve = serveStatic('./_gh_pages')
 const host = 'localhost'
-const port = 8080
+const port = 9001
 const screenshotsBase = './docs/4.0/examples/screenshots/'
+
+const options = {
+  screenSize: {
+    width: 1024,
+    height: 768
+  },
+  shotSize: {
+    width: 1024,
+    height: 768
+  },
+  renderDelay:3600
+}
 
 const items = [{
   src: `http://${host}:${port}/docs/4.0/examples/album/index.html`,
@@ -21,11 +33,18 @@ const items = [{
   src: `http://${host}:${port}/docs/4.0/examples/carousel/index.html`,
   dest: 'carousel.png'
 }, {
+  src: `http://${host}:${port}/docs/4.0/examples/checkout/index.html`,
+  dest: 'checkout.png'
+}, {
   src: `http://${host}:${port}/docs/4.0/examples/cover/index.html`,
   dest: 'cover.png'
 }, {
   src: `http://${host}:${port}/docs/4.0/examples/dashboard/index.html`,
   dest: 'dashboard.png'
+},
+{
+  src: `http://${host}:${port}/docs/4.0/examples/floating-labels/index.html`,
+  dest: 'floating-labels.png'
 },
 {
   src: `http://${host}:${port}/docs/4.0/examples/grid/index.html`,
@@ -37,34 +56,30 @@ const items = [{
 },
 {
   src: `http://${host}:${port}/docs/4.0/examples/narrow-jumbotron/index.html`,
-  dest: 'jumbotron-narrow.png'
+  dest: 'narrow-jumbotron.png'
 },
 {
-  src: `http://${host}:${port}/docs/4.0/examples/justified-nav/index.html`,
-  dest: 'justified-nav.png'
+  src: `http://${host}:${port}/docs/4.0/examples/quick-links/index.html`,
+  dest: 'quick-links.png'
 },
 {
-  src: `http://${host}:${port}/docs/4.0/examples/module-002-quick-links/index.html`,
-  dest: 'module-002-quick-links.png'
+  src: `http://${host}:${port}/docs/4.0/examples/content-panel/index.html`,
+  dest: 'content-panel.png'
 },
 {
-  src: `http://${host}:${port}/docs/4.0/examples/module-006-content-panel/index.html`,
-  dest: 'module-006-content-panel.png'
-},
-{
-  src: `http://${host}:${port}/docs/4.0/examples/module-012-product-details/index.html`,
-  dest: 'module-012-product-details.png'
+  src: `http://${host}:${port}/docs/4.0/examples/product-details/index.html`,
+  dest: 'product-details.png'
 },
 {
   src: `http://${host}:${port}/docs/4.0/examples/navbars/index.html`,
-  dest: 'navbar.png'
+  dest: 'navbars.png'
 },
 {
-  src: `http://${host}:${port}/docs/4.0/examples/navbar-top-fixed/index.html`,
+  src: `http://${host}:${port}/docs/4.0/examples/navbar-fixed/index.html`,
   dest: 'navbar-fixed.png'
 },
 {
-  src: `http://${host}:${port}/docs/4.0/examples/navbar-top-static/index.html`,
+  src: `http://${host}:${port}/docs/4.0/examples/navbar-static/index.html`,
   dest: 'navbar-static.png'
 },
 {
@@ -72,12 +87,28 @@ const items = [{
   dest: 'navbar-bottom.png'
 },
 {
+  src: `http://${host}:${port}/docs/4.0/examples/navbar-top/index.html`,
+  dest: 'navbar-top.png'
+},
+{
+  src: `http://${host}:${port}/docs/4.0/examples/navbar-top-fixed/index.html`,
+  dest: 'navbar-top-fixed.png'
+},
+{
   src: `http://${host}:${port}/docs/4.0/examples/offcanvas/index.html`,
   dest: 'offcanvas.png'
 },
 {
-  src: `http://${host}:${port}/docs/4.0/examples/signin/index.html`,
+  src: `http://${host}:${port}/docs/4.0/examples/sign-in/index.html`,
   dest: 'sign-in.png'
+},
+{
+  src: `http://${host}:${port}/docs/4.0/examples/pricing/index.html`,
+  dest: 'pricing.png'
+},
+{
+  src: `http://${host}:${port}/docs/4.0/examples/product/index.html`,
+  dest: 'product.png'
 },
 {
   src: `http://${host}:${port}/docs/4.0/examples/starter-template/index.html`,
@@ -96,28 +127,28 @@ const items = [{
   dest: 'tooltip-viewport.png'
 },
 {
-  src: `http://${host}:${port}/docs/4.0/examples/orange-admin/index.html`,
-  dest: 'example_admin.png'
+  src: `http://${host}:${port}/docs/4.0/examples/administration-mainpage/index.html`,
+  dest: 'administration-mainpage.png'
 },
 {
-  src: `http://${host}:${port}/docs/4.0/examples/orange-admin/edit.html`,
-  dest: 'example_admin_edit.png'
+  src: `http://${host}:${port}/docs/4.0/examples/administration-edition-page/index.html`,
+  dest: 'administration-edition-page.png'
 },
 {
-  src: `http://${host}:${port}/docs/4.0/examples/orange-dashboard/index.html`,
-  dest: 'example_dashboard.png'
+  src: `http://${host}:${port}/docs/4.0/examples/dashboard-orange-branded/index.html`,
+  dest: 'dashboard-orange-branded.png'
 },
 {
   src: `http://${host}:${port}/docs/4.0/examples/orange-form/index.html`,
-  dest: 'example_form.png'
+  dest: 'orange-form.png'
 },
 {
-  src: `http://${host}:${port}/docs/4.0/examples/orange-news/index.html`,
-  dest: 'example_news.png'
+  src: `http://${host}:${port}/docs/4.0/examples/news-template/index.html`,
+  dest: 'news-template.png'
 },
 {
-  src: `http://${host}:${port}/docs/4.0/examples/orange-brand-2015/index.html`,
-  dest: 'brand_2015.png'
+  src: `http://${host}:${port}/docs/4.0/examples/orange-homepage/index.html`,
+  dest: 'orange-homepage.png'
 }
 ]
 
@@ -132,7 +163,7 @@ server.on('listening', () => {
   console.log(`docs served on ${host}:${port}, let's take some pictures`)
 
   async.each(items, (item, callback) => {
-    webshot(item.src, screenshotsBase + item.dest, (err) => {
+    webshot(item.src, screenshotsBase + item.dest, options, (err) => {
       console.log(`url: ${item.src} in: ${screenshotsBase}${item.dest}`)
       if (err) {
         console.log(`oups something wen't wrong : ${err}`)
