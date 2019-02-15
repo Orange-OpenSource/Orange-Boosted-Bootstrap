cb6998df6dc3ce31ce3db15bb9415b1323955c87
Lionel MUKUNA CIOWELA <lionel.mukunaciowela@orange.com>
2019-02-15 18:20:40 +0100

    updates

--- package.js
+++ package.js
@@ -5,7 +5,7 @@
 Package.describe({
   name: 'Orange-OpenSource:Orange-Boosted-Bootstrap',  // https://atmospherejs.com/Orange-OpenSource:Orange-Boosted-Bootstrap
   summary: 'Orange Boosted with Bootstrap is a framework library accessible, ergonomic and Orange branded based on Bootstrap v4.2.1.',
-  version: '4.2.2',
+  version: '4.3.0',
   git: 'https://github.com/Orange-OpenSource/Orange-Boosted-Bootstrap.git'
 });
 
