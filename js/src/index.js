25e05ead4f0a3f21bc8239bd2a169e8c6ad34fc5
Gaël Poupard <gael.poupard@orange.com>
2020-06-24 15:21:36 +0200

    chore(merge v4-dev): last patched commit → 04dbb6c24

--- js/src/index.js
+++ js/src/index.js
@@ -21,7 +21,7 @@ import '../../node_modules/focus-visible/dist/focus-visible.js'
 /**
  * --------------------------------------------------------------------------
  * Bootstrap (v4.5.0): index.js
- * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
+ * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
  * --------------------------------------------------------------------------
  */
 
