2336e4711b61889a88db06b33f5972c2c8076bb6
Gaël Poupard <gael.poupard@orange.com>
2020-11-02 17:05:14 +0100

    chore(merge v4-dev): latest patched commit → 5664e728b

---
+++ js/index.js
@@ -0,0 +1,45 @@
+/**
+ * --------------------------------------------------------------------------
+ * Bootstrap (v4.5.3): index.js
+ * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
+ * --------------------------------------------------------------------------
+ */
+
+import Alert from './src/alert'
+import Button from './src/button'
+import Carousel from './src/carousel'
+import Collapse from './src/collapse'
+import Dropdown from './src/dropdown'
+import Modal from './src/modal'
+import Popover from './src/popover'
+import Scrollspy from './src/scrollspy'
+import Tab from './src/tab'
+import Toast from './src/toast'
+import Tooltip from './src/tooltip'
+import MegaMenu from './src/o-megamenu'
+import Navbar from './src/o-navbar'
+import Otab from './src/o-tab'
+import PriorityNav from './src/o-priority-nav'
+import ScrollUp from './src/o-scroll-up'
+import Util from './src/util'
+import '../node_modules/focus-visible/dist/focus-visible.js'
+
+export {
+  Util,
+  Alert,
+  Button,
+  Carousel,
+  Collapse,
+  Dropdown,
+  Modal,
+  Popover,
+  Scrollspy,
+  Tab,
+  Toast,
+  Tooltip,
+  MegaMenu,
+  Navbar,
+  Otab,
+  PriorityNav,
+  ScrollUp,
+}
