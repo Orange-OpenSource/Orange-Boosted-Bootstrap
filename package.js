5b2a05f08cf61647a1fbcdeba8ff6c9c5e5e6469
Gaël Poupard <gael.poupard@orange.com>
2020-11-05 16:05:52 +0100

    chore(merge v4-dev): latest patched commit → 44d54eb57

--- package.js
+++ package.js
@@ -1,17 +1,19 @@
 // package metadata file for Meteor.js
 
+/* eslint-env meteor */
+
 Package.describe({
-  name: 'Orange-OpenSource:Orange-Boosted-Bootstrap',  // https://atmospherejs.com/Orange-OpenSource:Orange-Boosted-Bootstrap
+  name: 'Orange-OpenSource:Orange-Boosted-Bootstrap', // https://atmospherejs.com/Orange-OpenSource:Orange-Boosted-Bootstrap
   summary: 'Orange Boosted with Bootstrap is a framework library accessible, ergonomic and Orange branded based on Bootstrap v4.5.3.',
   version: '4.5.3',
   git: 'https://github.com/Orange-OpenSource/Orange-Boosted-Bootstrap.git'
-});
+})
 
-Package.onUse(function (api) {
-  api.versionsFrom('METEOR@1.0');
-  api.use('jquery', 'client');
+Package.onUse(api => {
+  api.versionsFrom('METEOR@1.0')
+  api.use('jquery', 'client')
   api.addFiles([
     'dist/css/boosted.css',
     'dist/js/boosted.js'
-  ], 'client');
-});
+  ], 'client')
+})
