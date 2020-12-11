b22c24d70a1eae1b4012d5ef3a39bd0c66c7b692
Julien Déramond <julien.deramond@orange.com>
2020-12-11 08:09:31 +0100

    chore(storybook): replace CDN links to use 5.0.0-beta1 and use @storybook/addon-viewport

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,6 @@
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
+  viewport: { viewports: INITIAL_VIEWPORTS },
 }
