2a61373233147a30efdef1add2c07e9cc6ed68e5
Julien Déramond <julien.deramond@orange.com>
2021-09-06 16:26:25 +0200

    chore(release): v5.1.0

---
+++ docs/5.1/examples/sidebars/sidebars.js
@@ -0,0 +1,8 @@
+/* global boosted: false */
+(function () {
+  'use strict'
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  tooltipTriggerList.forEach(function (tooltipTriggerEl) {
+    new boosted.Tooltip(tooltipTriggerEl)
+  })
+})()
