ef3517cdd66644b8755a121451f0b8e651fa41f3
Julien Déramond <julien.deramond@orange.com>
2021-09-23 17:31:58 +0200

    e202996a2bb37c88379a2b4961e764b53dee54f2

--- build/vnu-jar.js
+++ build/vnu-jar.js
@@ -30,6 +30,10 @@ execFile('java', ['-version'], (error, stdout, stderr) => {
     // IE11 doesn't recognise <main> / give the element an implicit "main" landmark.
     // Explicit role="main" is redundant for other modern browsers, but still valid.
     'The “main” role is unnecessary for element “main”.',
+    // Per https://www.w3.org/TR/html-aria/#docconformance having "aria-disabled" on a link is
+    // NOT RECOMMENDED, but it's still valid - we explain in the docs that it's not ideal,
+    // and offer more robust alternatives, but also need to show a less-than-ideal example
+    'An “aria-disabled” attribute whose value is “true” should not be specified on an “a” element that has an “href” attribute.',
     // For some reason, the validator thinks the RTL carousel example is written in Catalan
     'This document appears to be written in .*',
     // Redundant ARIA landmarks role
