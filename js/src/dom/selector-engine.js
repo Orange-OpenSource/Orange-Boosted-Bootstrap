1042513c3322b6bf908b755582b454e47528ee4d
Julien Déramond <julien.deramond@orange.com>
2022-01-31 10:44:23 +0100

    5f1c542d677add524c94054ba8583269d81d87d0

--- js/src/dom/selector-engine.js
+++ js/src/dom/selector-engine.js
@@ -50,6 +50,7 @@ const SelectorEngine = {
     return []
   },
 
+  // TODO: this is now unused; remove later along with prev()
   next(element, selector) {
     let next = element.nextElementSibling
 
