5a0f85d8ffa3885fe58914449c22b26175bf4117
Julien Déramond <julien.deramond@orange.com>
2022-07-19 08:42:51 +0200

    .

--- site/assets/js/tac.js
+++ site/assets/js/tac.js
@@ -6,63 +6,6 @@
 (function () {
   'use strict'
 
-  window.addEventListener('tac.root_available', () => {
-    const tac = document.getElementById('tarteaucitron')
-
-    tac.querySelectorAll('.tarteaucitronAllow').forEach(button => {
-      button.classList.add('btn', 'btn-sm', 'btn-success', 'my-2', 'my-lg-0')
-      button.innerHTML = tarteaucitron.lang.allowAll
-    })
-
-    tac.querySelectorAll('.tarteaucitronDeny').forEach(button => {
-      button.classList.add('btn', 'btn-sm', 'btn-danger', 'my-2', 'my-lg-0', 'ms-lg-2')
-      button.innerHTML = tarteaucitron.lang.denyAll
-    })
-
-    document.getElementById('tarteaucitron').classList.add('p-3')
-
-    document.getElementById('tarteaucitronClosePanel').classList.add('d-flex', 'align-content-end', 'position-relative', 'btn-close')
-    document.getElementById('tarteaucitronClosePanel').innerHTML =
-      `<span class="visually-hidden">${document.getElementById('tarteaucitronClosePanel').innerHTML}</span>`
-  }, { once: true })
-
-  window.addEventListener('tac.open_alert', () => {
-    const alert = document.getElementById('tarteaucitronAlertBig')
-
-    document.getElementById('tarteaucitronCloseAlert').classList.add('btn', 'btn-sm', 'btn-info', 'btn-inverse', 'ms-lg-2')
-    alert.querySelector('.tarteaucitronAllow').classList.add('btn', 'btn-sm', 'btn-success', 'btn-inverse', 'mx-sm-2', 'ms-lg-auto', 'my-2', 'my-lg-0')
-    alert.querySelector('.tarteaucitronAllow').innerHTML = tarteaucitron.lang.acceptAll
-    alert.querySelector('.tarteaucitronDeny').classList.add('btn', 'btn-sm', 'btn-danger', 'btn-inverse', 'mx-sm-2', 'my-2', 'my-lg-0')
-    alert.querySelector('.tarteaucitronDeny').innerHTML = tarteaucitron.lang.denyAll
-  }, { once: true })
-
-  window.addEventListener('tac.open_panel',
-    () => {
-      document.querySelectorAll('#tarteaucitronServices_api button').forEach(button => {
-        button.classList.add('btn', 'btn-sm', 'ms-2')
-      })
-      const allowConsentButton = document.querySelector('#tarteaucitronAllAllowed')
-      const denyConsentButton = document.querySelector('#tarteaucitronAllDenied')
-      const checkboxes = document.querySelectorAll('.form-check-input')
-
-      allowConsentButton.addEventListener('click', () => {
-        for (let i = 0; i < checkboxes.length; i++) {
-          if (!checkboxes.checked) {
-            checkboxes[i].checked = true
-          }
-        }
-      })
-      denyConsentButton.addEventListener('click', () => {
-        for (let i = 0; i < checkboxes.length; i++) {
-          if (checkboxes.checked !== true) {
-            checkboxes[i].checked = false
-          }
-        }
-      })
-    },
-    { once: true }
-  )
-
   document.addEventListener(
     'googletagmanager_added',
     () => {
