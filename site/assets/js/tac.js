8527499650e53e36d08a6d57c9263253e5c1967b
Julien Déramond <julien.deramond@orange.com>
2022-07-19 09:09:33 +0200

    Add groupServices: false

--- site/assets/js/tac.js
+++ site/assets/js/tac.js
@@ -88,6 +88,7 @@ if (typeof tarteaucitron !== 'undefined') {
     hashtag: '#cookie-consent',
     cookieName: 'cookie-consent',
     orientation: 'bottom',
+    groupServices: false,
     bodyPosition: 'top',
     cookieslist: true,
     highPrivacy: false,
