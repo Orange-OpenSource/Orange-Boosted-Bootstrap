7cc775cb5edd914c7acdaf2ca94f98f0f4188927
louismaxime.piton <louismaxime.piton@orange.com>
2023-03-23 15:39:23 +0100

    Modify to better fit our current needs.

--- site/content/docs/5.3/examples/download-app/download-app.js
+++ site/content/docs/5.3/examples/download-app/download-app.js
@@ -1,10 +1,11 @@
 (() => {
   'use strict'
 
+  // The following code is used to redirect user directly on the download page of their store whether they have an Android or an iOS device.
   // https://stackoverflow.com/questions/35430336/redirect-users-to-itunes-app-store-or-google-play-store
-  if (!(/windows phone/i.test(navigator.userAgent)) && /android/i.test(navigator.userAgent)) {
-    window.location.href = 'https://play.google.com/apps/testing/com.orange.ods.app/'
-  } else if (/ipad|iphone|ipod/i.test(navigator.userAgent)) {
-    window.location.href = 'https://boosted.orange.com'
-  }
+  // if (!(/windows phone/i.test(navigator.userAgent)) && /android/i.test(navigator.userAgent)) {
+  //   window.location.href = 'https://play.google.com/apps/testing/com.orange.ods.app/'
+  // } else if (/ipad|iphone|ipod/i.test(navigator.userAgent)) {
+  //   window.location.href = 'https://boosted.orange.com'
+  // }
 })()
