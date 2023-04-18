670ae9d3ba4b2c1ce37c7b11206c07c79bdc1464
louismaxime.piton <louismaxime.piton@orange.com>
2023-04-18 18:53:04 +0200

    Animations on load

--- site/content/docs/5.3/examples/download-app/download-app.js
+++ site/content/docs/5.3/examples/download-app/download-app.js
@@ -5,7 +5,17 @@
   // https://stackoverflow.com/questions/35430336/redirect-users-to-itunes-app-store-or-google-play-store
   // if (!(/windows phone/i.test(navigator.userAgent)) && /android/i.test(navigator.userAgent)) {
   //   window.location.href = 'https://play.google.com/apps/testing/com.orange.ods.app/'
-  // } else if (/ipad|iphone|ipod/i.test(navigator.userAgent)) {
-  //   window.location.href = 'https://boosted.orange.com'
-  // }
+  if (/ipad|iphone|ipod/i.test(navigator.userAgent)) {
+    window.location.href = 'https://boosted.orange.com'
+  }
+
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('active')
+      }
+    })
+  })
+
+  observer.observe(document.querySelector('.images'))
 })()
