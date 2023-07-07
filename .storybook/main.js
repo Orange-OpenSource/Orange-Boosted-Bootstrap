04e351dcdea78df8fb02d529c6e6a33878ae8fcc
louismaxime.piton <louismaxime.piton@orange.com>
2023-07-07 10:02:27 +0200

    ?

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,6 @@
 // Note: addon-measure is forced to false in order to avoid it enabled in ZeroHeight
 const config = {
-  "stories": ["../stories/**/*.stories.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
+  "stories": ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
   "addons": ["@storybook/addon-a11y",
   "@storybook/addon-docs", {
     name: "@storybook/addon-essentials",
