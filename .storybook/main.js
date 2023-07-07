ed6b2096d372e2301204ed47c8ea57f2ed107dc3
louismaxime.piton <louismaxime.piton@orange.com>
2023-07-07 09:37:43 +0200

    Add docs tab ?

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,8 @@
 // Note: addon-measure is forced to false in order to avoid it enabled in ZeroHeight
 const config = {
   "stories": ["../stories/**/*.stories.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
-  "addons": ["@storybook/addon-a11y", {
+  "addons": ["@storybook/addon-a11y",
+  "@storybook/addon-docs", {
     name: "@storybook/addon-essentials",
     options: {
       measure: false
