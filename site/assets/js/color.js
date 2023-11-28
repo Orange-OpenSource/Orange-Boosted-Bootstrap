ebcde5ddeb56f23e4f82eea11ef1868d0f0e3786
louismaxime.piton <louismaxime.piton@orange.com>
2023-11-28 13:56:37 +0100

    Better UX

--- site/assets/js/color.js
+++ site/assets/js/color.js
@@ -9,13 +9,27 @@
  * For details, see https://creativecommons.org/licenses/by/3.0/.
  */
 
-/* global ClipboardJS: false */
+/* global ClipboardJS: false, boosted: false */
 
 (() => {
   'use strict'
 
-  new ClipboardJS('.color-copy', {
-    // target: trigger => trigger.closest('.bd-code-snippet').querySelector('.highlight'),
+  const copyVariable = new ClipboardJS('.color-copy', {
     text: trigger => trigger.getAttribute('data-clipboard-text')
+  })
+
+  copyVariable.on('success', event => {
+    const tooltipBtn = boosted.Tooltip.getInstance(event.trigger)
+    const originalTitle = event.trigger.getAttribute('data-bs-title')
+
+    tooltipBtn.setContent({ '.tooltip-inner': 'Copied!' })
+    event.trigger.addEventListener('hidden.bs.tooltip', () => {
+      tooltipBtn.setContent({ '.tooltip-inner': originalTitle })
+      event.trigger.title = originalTitle
+    }, { once: true })
+    event.trigger.addEventListener('mouseleave', () => {
+      tooltipBtn.hide()
+    }, { once: true })
+    event.clearSelection()
   })
 })()
