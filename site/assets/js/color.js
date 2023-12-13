3d3f578e527cb705e9e72837decd8c401fb98190
Julien Déramond <julien.deramond@orange.com>
2023-12-13 08:23:39 +0100

    .

--- site/assets/js/color.js
+++ site/assets/js/color.js
@@ -2,13 +2,6 @@
 // IT'S ALL JUST JUNK FOR OUR DOCS!
 // ++++++++++++++++++++++++++++++++++++++++++
 
-/*!
- * JavaScript for Bootstrap's docs (https://getbootstrap.com/)
- * Copyright 2011-2023 The Bootstrap Authors
- * Licensed under the Creative Commons Attribution 3.0 Unported License.
- * For details, see https://creativecommons.org/licenses/by/3.0/.
- */
-
 /* global ClipboardJS: false, boosted: false */
 
 (() => {
