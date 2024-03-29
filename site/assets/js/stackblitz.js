906c666a56b894ae7392fdb28ae17e2b6e8c73ec
Julien Déramond <julien.deramond@orange.com>
2024-03-29 06:59:39 +0100

    45fe28c5a6ed88d1a661355349aed799a79ed0fe

--- site/assets/js/stackblitz.js
+++ site/assets/js/stackblitz.js
@@ -54,7 +54,7 @@ const openBoostedSnippet = (htmlSnippet, jsSnippet, classes) => {
   <link href="${cssCdn}" rel="stylesheet">
   <link href="https://boosted.orange.com/docs/${docsVersion}/assets/css/docs.css" rel="stylesheet">
   <title>Boosted Example</title>
-  <${'script'} src="${jsBundleCdn}"></${'script'}>
+  <${'script'} defer src="${jsBundleCdn}"></${'script'}>
 </head>
 <body class="p-3 m-0 border-0 ${classes}">
   <!-- Example Code -->
