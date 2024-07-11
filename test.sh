4fbf3311dff09be067a24a18ca269640634ba2ea
Julien Déramond <julien.deramond@orange.com>
2024-07-11 13:47:59 +0200

    .

--- test.sh
+++ test.sh
@@ -1,5 +1,4 @@
-# find _site -name "*" -type f | xargs -I '{}' sed -i 's/\"\/docs/\"\/ouds-web\/docs/g'
-
-# find _site -type f | xargs -0 sed -i 's/\"\/docs/\"\/ouds-web\/docs/g'
-
-find _site -type f -print0 | xargs -0 sed -i "" 's/\"\/docs/\"\/ouds-web\/docs/g'
+mkdir ouds-web;
+mv _site/* ouds-web/;
+mv ouds-web _site/; 
+find _site -type f -print0 | xargs -0 sed -i "" 's/\"\/docs/\"\/ouds-web\/docs/g';
