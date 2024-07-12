2f79dc0f2b4ddd6856a4ee2118130904c5536811
Julien Déramond <julien.deramond@orange.com>
2024-07-12 11:12:54 +0200

    .

--- test.sh
+++ test.sh
@@ -1,5 +1,10 @@
 if [ -d ouds-web ]; then rm -rf ouds-web; fi;
 mkdir ouds-web;
 mv _site/* ouds-web/;
-mv ouds-web _site/; 
+mv ouds-web _site/;
+
+# Replace /docs with /ouds-web/docs
 find _site -type f -print0 | xargs -0 sed -i "" 's/\"\/docs/\"\/ouds-web\/docs/g';
+
+# Replace href="/" with href="/ouds-web"
+find _site -type f -print0 | xargs -0 sed -i "" 's/href=\"\/\"/href=\"\/ouds-web\"/g';
