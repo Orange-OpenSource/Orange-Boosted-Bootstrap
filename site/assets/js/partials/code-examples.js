48f952613886946b3469774d0b358933a1740fff
Julien Déramond <julien.deramond@orange.com>
2024-10-28 10:22:08 +0100

    5b0b7d07e8872519ab5d37ef9d59a854e206923a

--- site/assets/js/partials/code-examples.js
+++ site/assets/js/partials/code-examples.js
@@ -19,10 +19,11 @@ export default () => {
   const btnEdit = 'Edit on StackBlitz'
 
   // OUDS mod: removed unused svg height, width and fill="currentColor"
+  // OUDS mod: changed the positioning to improve the focus order
   const btnHtml = [
-    '<div class="bd-code-snippet">',
-    '  <div class="bd-clipboard">',
-    '    <button type="button" class="btn-clipboard">',
+    '<div class="bd-code-snippet position-relative">',
+    '  <div class="bd-clipboard position-absolute top-0 end-0">',
+    '    <button type="button" class="btn-clipboard position-absolute end-0">',
     '      <svg class="bi" role="img" aria-label="Copy"><use xlink:href="#clipboard"/></svg>',
     '    </button>',
     '  </div>',
@@ -35,7 +36,7 @@ export default () => {
       // Ignore examples made by shortcode
       if (!element.closest('.bd-example-snippet')) {
         element.insertAdjacentHTML('beforebegin', btnHtml)
-        element.previousElementSibling.append(element)
+        element.previousElementSibling.prepend(element)
       }
     })
 
