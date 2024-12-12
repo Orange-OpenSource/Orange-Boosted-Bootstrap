120b725529719bfc84060454c8fe5ab502597a5a
Louis-Maxime Piton <louismaxime.piton@orange.com>
2024-12-12 15:55:13 +0100

    chore(deps-dev): bump Storybook from 8.4.5 to 8.4.7 (#2817)

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,16 +3,12 @@ const config = {
   stories: [
     "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
   ],
-  addons: [
-    "@storybook/addon-a11y",
-    {
-      name: "@storybook/addon-essentials",
-      options: {
-        measure: false, // Note: addon-measure is forced to false in order to avoid it enabled in ZeroHeight
-      }
-    },
-    "@storybook/addon-themes",
-  ],
+  addons: ["@storybook/addon-a11y", {
+    name: "@storybook/addon-essentials",
+    options: {
+      measure: false, // Note: addon-measure is forced to false in order to avoid it enabled in ZeroHeight
+    }
+  }, "@storybook/addon-themes",],
   framework: {
     name: "@storybook/html-vite",
     options: {},
