fd1e4f5ffe113c90c66e04f9d050375c058d42f4
Maxime Lardenois <maxime.lardenois@orange.com>
2025-05-16 16:06:22 +0200

    chore: vnu again

--- build/vnu-jar.mjs
+++ build/vnu-jar.mjs
@@ -35,7 +35,7 @@ execFile('java', ['-version'], (error, stdout, stderr) => {
     // Poor aria-readonly handling see https://github.com/validator/validator/issues/1199
     'Attribute “aria-readonly” not allowed on element “span” at this point.',
     'Attribute “aria-readonly” not allowed on element “div” at this point.',
-    'error: Element “style” not allowed as child of element ”div” in this context. (Suppressing further errors from this subtree.)*'
+    'Element "style" not allowed as child of element "div" in this context. (Suppressing further errors from this subtree.)'
     // End mod
   ].join('|')
 
