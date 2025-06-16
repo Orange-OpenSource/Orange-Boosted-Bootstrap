3e4368e64f1f19c614bbbbcbdc486a044a3fae1b
Louis-Maxime Piton <louismaxime.piton@orange.com>
2025-06-16 16:14:10 +0200

    chore(release): v5.3.6 (#3021)
    ---------
    
    Co-authored-by: Vincent Prothais <vincent.prothais@orange.com>

--- js/dist/offcanvas.js
+++ js/dist/offcanvas.js
@@ -1,10 +1,10 @@
 /*!
-  * Boosted v5.3.5 (https://boosted.orange.com/)
+  * Boosted v5.3.6 (https://boosted.orange.com/)
   * Copyright 2015-2025 The Boosted Authors
   * Copyright 2015-2025 Orange
   * Licensed under MIT (https://github.com/orange-opensource/orange-boosted-bootstrap/blob/main/LICENSE)
   * This a fork of Bootstrap : Initial license below
-  * Bootstrap offcanvas.js v5.3.5 (https://boosted.orange.com/)
+  * Bootstrap offcanvas.js v5.3.6 (https://boosted.orange.com/)
   * Copyright 2011-2025 The Boosted Authors (https://github.com/Orange-OpenSource/Orange-Boosted-Bootstrap/graphs/contributors)
   * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
   */
