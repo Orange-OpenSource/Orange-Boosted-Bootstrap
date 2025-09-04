253caf60916295a9a86cd1352a8f96f6c3855307
Louis-Maxime Piton <louismaxime.piton@orange.com>
2025-09-04 09:52:05 +0200

    Better the doc brand switch

--- site/static/docs/[version]/assets/js/brand-picker.js
+++ site/static/docs/[version]/assets/js/brand-picker.js
@@ -7,30 +7,43 @@
 (() => {
   'use strict'
 
+  const url = new URLSearchParams(document.location.search)
   const getStoredBrand = () => localStorage.getItem('brand')
   const setStoredBrand = brand => localStorage.setItem('brand', brand)
 
   const getPreferredBrand = () => {
-    return getStoredBrand() || 'orange'
+    return url.get('brand') || getStoredBrand() || 'orange'
   }
 
   const setBrand = brand => {
     document.documentElement.setAttribute('data-bs-brand', brand)
+    url.set('brand', brand)
+    window.history.replaceState({}, '', `${window.location.pathname}?${url}`)
   }
 
   setBrand(getPreferredBrand())
 
-  const showActiveBrand = () => { // TODO: Finish this function.
+  const showActiveBrand = brand => {
     const brandSwitcher = document.querySelector('#bd-brand')
 
     if (!brandSwitcher) {
       return
     }
 
+    const btnToActive = document.querySelector(`[data-bs-brand-value="${brand}"]`)
+    const pathToActiveImage = btnToActive.querySelector('img').getAttribute('src')
+    const mainLogo = document.querySelector('.bd-header .navbar-brand img')
+
     document.querySelectorAll('[data-bs-brand-value]').forEach(element => {
       element.classList.remove('active')
       element.setAttribute('aria-pressed', 'false')
     })
+
+    brandSwitcher.setAttribute('aria-label', `Toggle brand (${brand})`)
+    brandSwitcher.querySelector('img').src = pathToActiveImage
+    btnToActive.classList.add('active')
+    btnToActive.setAttribute('aria-pressed', 'true')
+    mainLogo.src = pathToActiveImage
   }
 
   window.addEventListener('DOMContentLoaded', () => {
@@ -42,7 +55,7 @@
           const brand = toggle.getAttribute('data-bs-brand-value')
           setStoredBrand(brand)
           setBrand(brand)
-          showActiveBrand(brand, true)
+          showActiveBrand(brand)
         })
       })
   })
