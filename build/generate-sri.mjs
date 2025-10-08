0062c666dd327459d9d9346e8bdb9f9563385095
Louis-Maxime Piton <louismaxime.piton@orange.com>
2025-10-08 17:38:50 +0200

    .

--- build/generate-sri.mjs
+++ build/generate-sri.mjs
@@ -20,7 +20,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url))
 sh.config.fatal = true
 
 // eslint-disable-next-line unicorn/no-await-expression-member
-const BRANDS = (await fs.readdir('packages', { withFileTypes: true })).filter(file => file.isDirectory()).map(dir => dir.name)
+const BRANDS = fs.readdirSync('packages', { withFileTypes: true }).filter(file => file.isDirectory()).map(dir => dir.name)
 
 for (const brand of BRANDS) {
   const configFile = path.join(__dirname, `../packages/${brand}/config.yml`)
