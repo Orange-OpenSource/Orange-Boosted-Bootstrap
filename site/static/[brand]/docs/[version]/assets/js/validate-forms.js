5a6574834deb3a45fd4670ca6ad21354e762ab1d
Maxime Lardenois <maxime.lardenois@orange.com>
2026-03-03 21:55:10 +0100

    feat: handle correctly aria-describedby

--- site/static/[brand]/docs/[version]/assets/js/validate-forms.js
+++ site/static/[brand]/docs/[version]/assets/js/validate-forms.js
@@ -5,44 +5,50 @@
   function manageFeedbackMessage(field) {
     // Get the ID of the feedback message from the attribute data-feedback-id
     const feedbackId = field.getAttribute('data-feedback-id')
+    let description = field.getAttribute('aria-describedby') || ''
 
+    // aria-describedby can contain multiple space separated ids,
+    //  we need to preserve everything other than feedback id
+    description = description.replace(feedbackId, '').trim()
     if (feedbackId && field.checkValidity()) {
-      field.removeAttribute('aria-describedby')
+      // Valid field: remove feedback id from aria-describedby attribute string
+      if (!description) {
+        field.removeAttribute('aria-describedby')
+        return
+      }
     } else if (!field.checkValidity()) {
-      field.setAttribute('aria-describedby', feedbackId)
+      // Add feedback id on aria-describedby if field invalid
+      description = `${feedbackId} ${description}`
     }
+
+    // Apply update on aria-describedby
+    field.setAttribute('aria-describedby', description)
   }
 
-  let inputListenersAdded = false
-
-  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  // Fetch all the forms we want to apply custom OUDS Web validation styles to
   const forms = document.querySelectorAll('.needs-validation')
 
   // Loop over them and prevent submission
-  Array.from(forms).forEach(form => {
+  forms.forEach(form => {
     // Gets all the fields of the form (input, select, textarea)
     const fields = form.querySelectorAll('input, select, textarea')
 
-    form.addEventListener('submit', event => {
-      // Initially manages feedback messages and add input listeners
-      if (!inputListenersAdded) {
-        fields.forEach(field => {
-          manageFeedbackMessage(field)
+    // Initially manages feedback messages and add input listeners
+    fields.forEach(field => {
+      field.addEventListener('input', () => {
+        manageFeedbackMessage(field)
+      })
+    })
 
-          // eslint-disable-next-line max-nested-callbacks
-          field.addEventListener('input', () => {
-            manageFeedbackMessage(field)
-          })
-        })
-        inputListenersAdded = true
-      }
+    form.addEventListener('submit', event => {
+      fields.forEach(field => {
+        manageFeedbackMessage(field)
+      })
 
       if (!form.checkValidity()) {
         event.preventDefault()
         event.stopPropagation()
       }
-
-      form.classList.add('was-validated')
     }, false)
   })
 })()
