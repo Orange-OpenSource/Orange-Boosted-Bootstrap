f9e2281eb9ad168bfe1b66e9c556f5517eea8db3
Louis-Maxime Piton <louismaxime.piton@orange.com>
2026-04-03 13:05:24 +0200

    feat(lib): Add Footer as a draft component (#3389)
    Co-authored-by: Maxime Lardenois <maxime.lardenois@orange.com>
    Co-authored-by: Vincent Prothais <vincent.prothais@orange.com>

--- site/data/components-details.ts
+++ site/data/components-details.ts
@@ -100,10 +100,42 @@ export const componentsDetails: ComponentCardData[] = [
       </ul>`
   },
   {
+    name: 'Footer',
+    category: 'Navigation',
+    snippet: `
+      <footer class="footer navbar w-75" data-bs-theme="dark">
+        <h2 class="visually-hidden">Sitemap &amp; information</h2>
+        <div class="container-fluid container-max-width footer-nav px-small py-2xl-small d-lg-none d-sm-none d-md-block">
+          <nav class="accordion">
+            <div class="row">
+              <div class="footer-column">
+                <h3 class="accordion-header footer-heading">
+                  <button class="accordion-button collapsed container-fluid container-max-width px-none d-md-none" type="button">Label</button>
+                  <span class="d-none d-md-flex">Label</span>
+                </h3>
+                <div class="container-fluid container-max-width accordion-collapse collapse">
+                  <ul class="navbar-nav ps-large ps-md-none">
+                    <li><a class="nav-link" href="#">Label</a></li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </nav>
+        </div>
+        <div class="border-bottom border-thin border-default d-lg-none d-sm-none d-md-block"></div>
+        <div class="container-fluid container-max-width footer-terms px-small">
+          <ul class="navbar-nav gap-md-large">
+            <li>© Label</li>
+            <li><a class="nav-link" href="#">Label</a></li>
+          </ul>
+        </div>
+      </footer>`
+  },
+  {
     name: 'Header',
     category: 'Navigation',
     snippet: `
-      <header class="header-minimized">
+      <header class="header-minimized w-75">
         <nav class="navbar navbar-expand-lg">
           <div class="container-fluid container-max-width px-small">
             <div class="navbar-brand me-auto me-lg-xlarge">
