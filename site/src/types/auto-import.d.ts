bc4d47e3322994015eba365551dd96aa0ef6788e
Louis-Maxime Piton <louismaxime.piton@orange.com>
2026-07-01 10:31:32 +0200

    Test for css

--- site/src/types/auto-import.d.ts
+++ site/src/types/auto-import.d.ts
@@ -28,6 +28,5 @@ export declare global {
   export const SvgDocs: typeof import('@shortcodes/SvgDocs.astro').default
   export const Table: typeof import('@shortcodes/Table.astro').default
   export const ThemeDemo: typeof import('@shortcodes/ThemeDemo.astro').default
-  export const ThemeDemoStyles: typeof import('@shortcodes/ThemeDemoStyles.astro').default
-  export const ThemeDemoStylesProd: typeof import('@shortcodes/ThemeDemoStylesProd.astro').default
+  export const ThemeDemoStylesImport: typeof import('@shortcodes/ThemeDemoStylesImport.astro').default
 }
