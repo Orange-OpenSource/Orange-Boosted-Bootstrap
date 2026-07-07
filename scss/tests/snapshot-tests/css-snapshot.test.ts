9e3cc19f0bd2ebf61b2b621795183bf8cfa00167
Maxime Lardenois <maxime.lardenois@orange.com>
2026-07-07 16:47:52 +0200

    add bootstrap compat

--- scss/tests/snapshot-tests/css-snapshot.test.ts
+++ scss/tests/snapshot-tests/css-snapshot.test.ts
@@ -4,11 +4,17 @@ import {
   describe, it, expect
 } from 'vitest'
 
-const cssFile = path.resolve(__dirname, '../../../packages/orange/dist/css/ouds-web.css')
-const snapshotFile = path.resolve(__dirname, '__snapshots__/ouds-web.css')
-
 describe('Orange CSS build', () => {
-  it('should build CSS matching the snapshot', async () => {
+  it('should build ouds-web matching the snapshot', async () => {
+    const cssFile = path.resolve(__dirname, '../../../packages/orange/dist/css/ouds-web.css')
+    const snapshotFile = path.resolve(__dirname, '__snapshots__/ouds-web.css')
+    const css = await fs.readFile(cssFile, 'utf8')
+
+    await expect(css).toMatchFileSnapshot(snapshotFile)
+  })
+  it('should build ouds-web-bootstrap matching the snapshot', async () => {
+    const cssFile = path.resolve(__dirname, '../../../packages/orange/dist/css/ouds-web-bootstrap.css')
+    const snapshotFile = path.resolve(__dirname, '__snapshots__/ouds-web-bootstrap.css')
     const css = await fs.readFile(cssFile, 'utf8')
 
     await expect(css).toMatchFileSnapshot(snapshotFile)
