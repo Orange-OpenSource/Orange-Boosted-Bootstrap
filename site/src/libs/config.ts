17639f83b3ae9fd2fbf164f6efb5a9e249059d66
Louis-Maxime Piton <louismaxime.piton@orange.com>
2026-08-05 12:25:54 +0200

    Bump several packages

--- site/src/libs/config.ts
+++ site/src/libs/config.ts
@@ -1,6 +1,6 @@
 import fs from 'node:fs'
 import path from 'node:path'
-import yaml from 'js-yaml'
+import * as yaml from 'js-yaml'
 import { z } from 'zod'
 import { zVersionMajorMinor, zVersionSemver } from './validation'
 
