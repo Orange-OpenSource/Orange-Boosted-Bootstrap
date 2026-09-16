bfb1618fbf12d16545ac7ebfdb007dc8c936a56b
Alexandra Duval <alexandra.duval@orange.com>
2026-09-16 17:21:38 +0200

    feat: refactored loading and disabled states into their own component

--- site/data/components-details.ts
+++ site/data/components-details.ts
@@ -181,7 +181,7 @@ export const componentsDetails: ComponentCardData[] = [
     category: 'Actions',
     link: 'icons',
     snippet: `
-      <button class="icon-interactive">
+      <button class="icon icon-interactive bl-large-icon">
         <svg aria-hidden="true">
           <use xlink:href="${getVersionedDocsPath('/assets/img/ouds-web-sprite.svg#heart-empty')}"/>
         </svg>
